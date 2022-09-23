a14b1f54e9c83c1b24409962d085161d06e4fdb9
jonphamm <jonpham09@gmail.com>
2022-09-23 16:15:14 -0400

    Add new book form pop up

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// button to create new book, add new book to page, and close pop
+const addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', addBookToLibrary);
+
+const newBookBtn = document.querySelector('#newBtn');
+newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
+
+const popUpForm = document.getElementById('popUp');
+const closePopUp = document.getElementsByTagName('span')[0];
+closePopUp.addEventListener('click', () => popUpForm.style.display = 'none')
+
+
 // declare empty array for library
 let myLibrary = [];
 
@@ -14,7 +26,7 @@ function Book(title, author, pages, read) {
 
 // function for adding new book to array
 function addBookToLibrary() {
-    
+
 }
 
 const theHobbit = new Book('The Hobbit', ' by J.J.', '259 pages', 'not read yet.');
