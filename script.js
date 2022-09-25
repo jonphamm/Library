a0cf708be9d8ef6f40b0f45b7aa806faa11d633d
jonphamm <jonpham09@gmail.com>
2022-09-25 12:07:57 -0400

    Create new book display buttons

--- script.js
+++ script.js
@@ -1,35 +1,69 @@
-// button to create new book, add new book to page, and close pop
+// button to ADD new book to page
 const addBtn = document.querySelector('#addBtn');
 addBtn.addEventListener('click', addBookToLibrary);
 
+// button to CREATE new book
 const newBookBtn = document.querySelector('#newBtn');
 newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
 
+// button to CLOSE form pop up
 const popUpForm = document.getElementById('popUp');
 const closePopUp = document.getElementsByTagName('span')[0];
 closePopUp.addEventListener('click', () => popUpForm.style.display = 'none')
 
-
-// declare empty array for library
-let myLibrary = [];
-
-// object constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return (title + author + ', ' + pages + ', ' + read);
+// Book constructor
+class Book {
+    constructor(title, author, pages, read) {
+    this.title = form.title.value;
+    this.author = form.author.value;
+    this.pages = form.pages.value + 'pg';
+    this.read = form.read.checked;
     }
 }
 
-// function for adding new book to array
+// creates book from Book Constructor, adds to library
+let myLibrary = [];
+let newBook;
+
+// function for adding new book to array/library
 function addBookToLibrary() {
+    event.preventDefault();
+    popUpForm.style.display = 'none';
+
+    newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook); // adds 'newBook' constructor to empty 'myLibrary' array
+    setData(); // saves updated array in local storage
+    render();
+    form.reset(); // resets all the values in the form
+}
+
+// creates book visual in browser
+function render() {
+    const display = document.getElementById('library-container');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild('book')); // removes the child node 'book' from the DOM and returns the removed node
+
+    for(let i = 0; i < myLibrary; i++) {
+        createBook(myLibrary[i]); 
+    }
+}
+
+// creates book DOM elements to use in render() function
+function createBook(item) {
+    const library = document.querySelector('#library-container');
+    const bookDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const pagesDiv = document.createElement('div');
+    const removeDiv = document.createElement('div');
+    const readDiv = document.createElement('div');
+
+    bookDiv.classList.add('book'); 
+    bookDiv.setAttribute('id', myLibrary.indexOf(item)); // indexOf returns the first index at which a given element can be found in the array
 
 }
 
-const theHobbit = new Book('The Hobbit', ' by J.J.', '259 pages', 'not read yet.');
-console.log(theHobbit.info());
-
-
+// setting Library to be stored in local storage
+function setData() {
+    localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
+}
